3103cfa8700e03d3f1a72477b0e8f562ee4e81b6
jamshidnoor <44399792+jamshidnoor@users.noreply.github.com>
2023-01-04 21:24:43 -0500

    Update GitPratice.java
    This is from remote

--- src/gitpratice/GitPratice.java
+++ src/gitpratice/GitPratice.java
@@ -3,6 +3,11 @@ package gitpratice;
 public class GitPratice {
 
 	public static void main(String[] args) {
+		System.out.println("this is from local repo");
+	}
+	
+	public static void main(String[] args) {
+System.out.println("this is from remote local repo");
 	}
 
 }
